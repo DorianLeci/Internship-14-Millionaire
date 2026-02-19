a90f0d99211d339938b0bb86e64a2457b4534f80
DorianLeci <dleci00@fesb.hr>
2026-02-19 17:37:08 +0100

    Add data folder with "Question" and "Answer" class

---
+++ Millionare/src/data/Question.js
@@ -0,0 +1,6 @@
+export class Question{
+    cosntructor(question,answers){
+        this.question=question;
+        this.answers=answers;
+    }
+}
---
+++ Millionare/src/data/questions.js
@@ -0,0 +1,216 @@
+import { Question } from "./Question.js";
+import { Answer } from "./Answer.js";
+
+export const questions = [
+
+  new Question("Koji je glavni grad Hrvatske?", [
+    new Answer("Split", false),
+    new Answer("Zagreb", true),
+    new Answer("Rijeka", false),
+    new Answer("Osijek", false),
+  ]),
+
+  new Question("Koliko kontinenata postoji?", [
+    new Answer("5", false),
+    new Answer("6", false),
+    new Answer("7", true),
+    new Answer("8", false),
+  ]),
+
+  new Question("Koji planet je najbliži Suncu?", [
+    new Answer("Venera", false),
+    new Answer("Merkur", true),
+    new Answer("Mars", false),
+    new Answer("Jupiter", false),
+  ]),
+
+  new Question("Koji je najveći ocean na Zemlji?", [
+    new Answer("Atlantski", false),
+    new Answer("Indijski", false),
+    new Answer("Tihi", true),
+    new Answer("Arktički", false),
+  ]),
+
+  new Question("Koliko dana ima prijestupna godina?", [
+    new Answer("365", false),
+    new Answer("366", true),
+    new Answer("364", false),
+    new Answer("360", false),
+  ]),
+
+  new Question("Što React koristi za efikasno ažuriranje DOM-a?", [
+    new Answer("Shadow DOM", false),
+    new Answer("Virtual DOM", true),
+    new Answer("Real DOM Cache", false),
+    new Answer("HTML Diff Engine", false),
+  ]),
+
+  new Question("Koja je najmanja država na svijetu?", [
+    new Answer("Monako", false),
+    new Answer("Vatikan", true),
+    new Answer("San Marino", false),
+    new Answer("Malta", false),
+  ]),
+
+  new Question("Koji je kemijski simbol za zlato?", [
+    new Answer("Ag", false),
+    new Answer("Au", true),
+    new Answer("Fe", false),
+    new Answer("Zn", false),
+  ]),
+
+  new Question("Koji je najveći sisavac na svijetu?", [
+    new Answer("Slon", false),
+    new Answer("Plavi kit", true),
+    new Answer("Žirafa", false),
+    new Answer("Nosorog", false),
+  ]),
+
+  new Question("Koja je najduža rijeka na svijetu?", [
+    new Answer("Amazona", false),
+    new Answer("Nil", true),
+    new Answer("Dunav", false),
+    new Answer("Mississippi", false),
+  ]),
+
+  new Question("Koliko ima igrača na terenu ima u nogometnoj ekipi?", [
+    new Answer("9", false),
+    new Answer("10", false),
+    new Answer("11", true),
+    new Answer("12", false),
+  ]),
+
+  new Question("Koji je najveći planet Sunčevog sustava?", [
+    new Answer("Saturn", false),
+    new Answer("Jupiter", true),
+    new Answer("Neptun", false),
+    new Answer("Mars", false),
+  ]),
+
+  new Question("Koji HTML tag se koristi za link?", [
+    new Answer("<div>", false),
+    new Answer("<a>", true),
+    new Answer("<p>", false),
+    new Answer("<span>", false),
+  ]),
+
+  new Question("Koji je rezultat izraza 5x6?", [
+    new Answer("25", false),
+    new Answer("30", true),
+    new Answer("35", false),
+    new Answer("40", false),
+  ]),
+
+  new Question("Koja država ima oblik čizme?", [
+    new Answer("Španjolska", false),
+    new Answer("Italija", true),
+    new Answer("Grčka", false),
+    new Answer("Portugal", false),
+  ]),
+
+  new Question("Koja je najbrža kopnena životinja?", [
+    new Answer("Lav", false),
+    new Answer("Gepard", true),
+    new Answer("Tigar", false),
+    new Answer("Konj", false),
+  ]),
+
+  new Question("Koji je glavni grad Francuske?", [
+    new Answer("Lyon", false),
+    new Answer("Pariz", true),
+    new Answer("Marseille", false),
+    new Answer("Nice", false),
+  ]),
+
+  new Question("Koji uređaj se koristi za mjerenje temperature?", [
+    new Answer("Barometar", false),
+    new Answer("Termometar", true),
+    new Answer("Higrometar", false),
+    new Answer("Voltmeter", false),
+  ]),
+
+  new Question("Koji je najveći kontinent?", [
+    new Answer("Europa", false),
+    new Answer("Afrika", false),
+    new Answer("Azija", true),
+    new Answer("Australija", false),
+  ]),
+
+  new Question("Koji broj je binarno zapisan kao 101?", [
+    new Answer("3", false),
+    new Answer("4", false),
+    new Answer("5", true),
+    new Answer("6", false),
+  ]),
+
+  new Question("Koji CSS property mijenja boju teksta?", [
+    new Answer("background", false),
+    new Answer("font-style", false),
+    new Answer("color", true),
+    new Answer("border", false),
+  ]),
+
+  new Question("Koji je najveći otok na svijetu?", [
+    new Answer("Madagaskar", false),
+    new Answer("Grenland", true),
+    new Answer("Island", false),
+    new Answer("Borneo", false),
+  ]),
+
+  new Question("Koliko sati ima jedan dan?", [
+    new Answer("12", false),
+    new Answer("18", false),
+    new Answer("24", true),
+    new Answer("48", false),
+  ]),
+
+  new Question("Koji je simbol za vodu u kemiji?", [
+    new Answer("CO2", false),
+    new Answer("H2O", true),
+    new Answer("O2", false),
+    new Answer("NaCl", false),
+  ]),
+
+  new Question("Koji framework upotrebljava JSX?", [
+    new Answer("Angular", false),
+    new Answer("React", true),
+    new Answer("Vue", false),
+    new Answer("Laravel", false),
+  ]),
+
+  new Question("Koja planet je poznat kao Crveni planet?", [
+    new Answer("Mars", true),
+    new Answer("Jupiter", false),
+    new Answer("Venera", false),
+    new Answer("Saturn", false),
+  ]),
+
+  new Question("Koji je najveći broj u ovom nizu?", [
+    new Answer("102", false),
+    new Answer("120", false),
+    new Answer("1002", true),
+    new Answer("999", false),
+  ]),
+
+  new Question("Koji dio računala služi za obradu podataka?", [
+    new Answer("RAM", false),
+    new Answer("CPU", true),
+    new Answer("SSD", false),
+    new Answer("GPU", false),
+  ]),
+
+  new Question("Koji je najveći organ u ljudskom tijelu?", [
+    new Answer("Srce", false),
+    new Answer("Jetra", false),
+    new Answer("Koža", true),
+    new Answer("Pluća", false),
+  ]),
+
+  new Question("Koji je rezultat izraza 9x9?", [
+    new Answer("72", false),
+    new Answer("81", true),
+    new Answer("99", false),
+    new Answer("69", false),
+  ]),
+
+];
