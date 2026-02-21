5d9b014c8a7b3c80a931cb0c8ecbfab9be720013
DorianLeci <dleci00@fesb.hr>
2026-02-21 22:31:36 +0100

    Update "JokerContainer" styling and animations

--- Millionare/src/components/JokerContainer/JokerContainer.jsx
+++ Millionare/src/components/JokerContainer/JokerContainer.jsx
@@ -3,6 +3,8 @@ import { JokerType } from "../../enums/JokerType.js";
 import { gameManager } from "../../GameLogic/GameManager";
 import "./JockerContainer.css";
 import fiftyIcon from "../../assets/images/50-percent-icon.svg";
+import skipIcon from "../../assets/images/skip-icon.svg";
+import swapIcon from "../../assets/images/swap-icon.svg";
 
 export function JokerContainer() {
   const [isJokerUsed, setIsJokerUsed] = useState(
@@ -30,17 +32,36 @@ export function JokerContainer() {
   console.log(isJokerUsed[JokerType.FIFTY_FIFTY]);
   return (
     <div className="joker-container">
-      <button
-        className="joker-button"
-        onClick={() => onJokerClick(JokerType.FIFTY_FIFTY)}
-        disabled={isJokerUsed[JokerType.FIFTY_FIFTY]}
-      >
-        <img
-          className="fifty-icon"
-          src={fiftyIcon}
-          alt="FiftyFifty Joker"
-        ></img>
-      </button>
+      <h2 className="joker-title">Jokers</h2>
+      <div class="joker-buttons">
+        <button
+          className="joker-button"
+          onClick={() => onJokerClick(JokerType.FIFTY_FIFTY)}
+          disabled={isJokerUsed[JokerType.FIFTY_FIFTY]}
+        >
+          <img
+            className="icon fifty-icon"
+            src={fiftyIcon}
+            alt="FiftyFifty Joker"
+          ></img>
+        </button>
+
+        <button
+          className="joker-button"
+          onClick={() => onJokerClick(JokerType.SKIP_QUESTION)}
+          disabled={isJokerUsed[JokerType.SKIP_QUESTION]}
+        >
+          <img className="icon skip-icon" src={skipIcon} alt="Skip Joker"></img>
+        </button>
+
+        <button
+          className="joker-button"
+          onClick={() => onJokerClick(JokerType.SWAP_QUESTION)}
+          disabled={isJokerUsed[JokerType.SWAP_QUESTION]}
+        >
+          <img className="icon swap-icon" src={swapIcon} alt="Swap Joker"></img>
+        </button>
+      </div>
     </div>
   );
 }
